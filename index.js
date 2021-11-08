9a63d8d83925efc62b54d4bc81a622f43194f085
Luis Locon <luis.locon@cognits.co>
2021-11-08 13:28:31 -0600

    cleaning code

--- index.js
+++ index.js
@@ -43,28 +43,3 @@ app.post("/api/email", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-/**
- * {
-  city: 'Guatemala',
-  company: 'cognits, S.A.',
-  config: {
-    color: 'Silver',
-    footing: '5000 - 10000',
-    hexColor: '#e9e9e9',
-    imageURI: 'ww',
-    materialType: 'Galvanized Steel',
-    painType: 'Metallic Colors',
-    placement: 'Walls',
-    thickness: '12G',
-    title: 'V-Beam'
-  },
-  date: '2021-11-18',
-  email: 'luis@example.com',
-  lastname: 'Locon',
-  name: 'Luis',
-  state: 'Guatemala',
-  street: 'Guatemala'
-}
- */
